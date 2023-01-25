7e4eff66f43072a688ad3db2f871c554068d52ba
Henri vanhuynegem <93914927+Hvanhuynegem@users.noreply.github.com>
2023-01-25 16:41:00 +0100

    set up everythin for express server

---
+++ .history/app_20230125164027.js
@@ -0,0 +1,9 @@
+// initialize express
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+
+
